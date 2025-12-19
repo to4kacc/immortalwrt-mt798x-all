a788aa48278c4dcbbe0a5a2193db88521d399a9d
panther706 <43943311+panther706@users.noreply.github.com>
2025-12-19 17:40:01 +0800

    Update mtwifi.sh

--- package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+++ package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -20,7 +20,7 @@ detect_mtwifi() {
 					hwmode="11g"
 					htmode="HE40"
 					htbsscoex="1"
-					ssid="ZeroWrt-2.4G"
+					ssid="ImmortalWrt-2.4G"
 					dbdc_main="1"
 					txpower="100"
 					channel="auto"
@@ -29,7 +29,7 @@ detect_mtwifi() {
 					hwmode="11a"
 					htmode="HE160"
 					htbsscoex="0"
-					ssid="ZeroWrt-5G"
+					ssid="ImmortalWrt-5G"
 					channel="36"
 					txpower="100"
 					dbdc_main="0"
@@ -54,8 +54,8 @@ detect_mtwifi() {
 					set wireless.default_${dev}.network=lan
 					set wireless.default_${dev}.mode=ap
 					set wireless.default_${dev}.ssid=${ssid}
-					set wireless.default_${dev}.encryption=psk2+ccmp
-					set wireless.default_${dev}.key=1234567890
+					set wireless.default_${dev}.encryption=none
+				
 EOF
 				uci -q commit wireless
 			}
